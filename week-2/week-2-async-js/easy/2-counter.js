9bf500fb202121edd5e3704ccd9d333a3bc74416
Aditya Vishal Joshi <aditya.cloud.expert@gmail.com>
2024-11-18 18:26:29 +0530

    Assignments

---
+++ week-2/week-2-async-js/easy/2-counter.js
@@ -0,0 +1,13 @@
+// ## Counter without setInterval
+
+// Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck.
+
+let count = 0;
+
+function counter() {
+    console.log(count);
+    count++;
+    setTimeout(counter, 1000); // Recursively call the function after 1 second
+}
+
+counter(); // Start the counter
