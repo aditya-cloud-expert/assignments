c2c04904e65c6753998d9dc2f0c9d4cba813f670
Aditya Vishal Joshi <aditya.cloud.expert@gmail.com>
2024-12-07 11:15:12 +0530

    random commit

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -5,7 +5,33 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length != str2.length ) return false;
 
+  let m = new Map();
+
+  for(let i = 0 ; i < str1.length ; i++)
+  {
+    if(m.find(str1[i]))
+    {
+      m[str1[i]]+=1
+    }
+    else
+    {
+      m[str1[i]] = 1;
+    }
+  }
+
+  for(let i = 0 ; i < str2.length ; i++)
+    {
+      if(m.find(str1[i]))
+      {
+        m[str1[i]]+=1
+      }
+      else
+      {
+        m[str1[i]] = 1;
+      }
+    }
 }
 
 module.exports = isAnagram;
