5231dae88765d9bb22a7776759a3f405fa9de581
Aditya Vishal Joshi <aditya.cloud.expert@gmail.com>
2024-12-07 19:34:46 +0530

    Update todo list

--- week-2/week-2-js/hard/todo-list.js
+++ week-2/week-2-js/hard/todo-list.js
@@ -1,17 +1,50 @@
-/*
-  Implement a class `Todo` having below methods
-    - add(todo): adds todo to list of todos
-    - remove(indexOfTodo): remove todo from list of todos
-    - update(index, updatedTodo): update todo at given index
-    - getAll: returns all todos
-    - get(indexOfTodo): returns todo at given index
-    - clear: deletes all todos
-
-  Once you've implemented the logic, test your code by running
-*/
-
 class Todo {
-    
+  constructor() {
+    this.list = []; // Initialize the todos list as an empty array
+  }
+
+  // Adds a todo to the list
+  add(todo) {
+    this.list.push(todo);
+  }
+
+  // Removes a todo by index
+  remove(index) {
+    if (index >= 0 && index < this.list.length) {
+      this.list.splice(index, 1); // Use splice to remove the item at the given index
+    } else {
+      console.error("Invalid index. Unable to remove item.");
+    }
+  }
+
+  // Updates a todo at the given index
+  update(index, updatedTodo) {
+    if (index >= 0 && index < this.list.length) {
+      this.list[index] = updatedTodo;
+    } else {
+      console.error("Invalid index. Unable to update item.");
+    }
+  }
+
+  // Returns all todos
+  getAll() {
+    return this.list;
+  }
+
+  // Returns a todo at the given index
+  get(index) {
+    if (index >= 0 && index < this.list.length) {
+      return this.list[index];
+    } else {
+      console.error("Invalid index. Unable to retrieve item.");
+      return null;
+    }
+  }
+
+  // Clears all todos
+  clear() {
+    this.list = [];
+  }
 }
 
 module.exports = Todo;
