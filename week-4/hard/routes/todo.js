a1c280e0b92bf1ad751ee601b724233ead474d82
Aditya Vishal Joshi <aditya.cloud.expert@gmail.com>
2024-12-09 18:05:11 +0530

    Update Index.js

--- week-4/hard/routes/todo.js
+++ week-4/hard/routes/todo.js
@@ -1,31 +1,32 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/user");
-const router = Router();
+const todorouter = Router();
+
 
 // todo Routes
-router.post('/', (req, res) => {
+todorouter.post('/', (req, res) => {
     // Implement todo creation logic
 });
 
-router.put('/', adminMiddleware, (req, res) => {
+todorouter.put('/', adminMiddleware, (req, res) => {
     // Implement update todo  logic
 });
 
-router.delete('/', adminMiddleware, (req, res) => {
+todorouter.delete('/', adminMiddleware, (req, res) => {
     // Implement delete todo logic
 });
 
-router.delete('/:id', adminMiddleware, (req, res) => {
+todorouter.delete('/:id', adminMiddleware, (req, res) => {
     // Implement delete todo by id logic
 });
 
 
-router.get('/', adminMiddleware, (req, res) => {
+todorouter.get('/', adminMiddleware, (req, res) => {
     // Implement fetching all todo logic
 });
 
-router.get('/:id', adminMiddleware, (req, res) => {
+todorouter.get('/:id', adminMiddleware, (req, res) => {
     // Implement fetching todo by id logic
 });
 
-module.exports = router;
+module.exports = todorouter;
--- week-4/hard/routes/user.js
+++ week-4/hard/routes/user.js
@@ -1,22 +1,22 @@
 const { Router } = require("express");
-const router = Router();
+const userrouter = Router();
 const userMiddleware = require("../middleware/user");
 
 // User Routes
-router.post('/signup', (req, res) => {
+userrouter.post('/signup', (req, res) => {
     // Implement user signup logic
 });
 
-router.post('/login', (req, res) => {
+userrouter.post('/login', (req, res) => {
      // Implement user login logic
 });
 
-router.get('/todos', userMiddleware, (req, res) => {
+userrouter.get('/todos', userMiddleware, (req, res) => {
     // Implement logic for getting todos for a user
 });
 
-router.post('/logout', userMiddleware, (req, res) => {
+userrouter.post('/logout', userMiddleware, (req, res) => {
     // Implement logout logic
 });
 
-module.exports = router
+module.exports = userrouter
