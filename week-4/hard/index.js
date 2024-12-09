745f89ff750690e41bde6d52ae90f9b368009df0
Aditya Vishal Joshi <aditya.cloud.expert@gmail.com>
2024-12-09 10:44:12 +0530

    Update Index.js

--- week-4/hard/index.js
+++ week-4/hard/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
-
+const { todo, use } = require("./routes/todo")
+const { user } = require("./routes/user")
 const app = express();
 const port = process.env.PORT;
+
 
 app.use(express.json());
 
 app.get("/healthy", (req, res)=> res.send("I am Healthy"));
+app.use("/todo", todo)
+app.use("/user", user)
 
 //  start writing your routes here
 
